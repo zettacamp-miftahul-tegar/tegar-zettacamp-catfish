bc21c86fc13598031ab603ab656fc163ee836b55
MiftahulTegar <miftahul.tegar@zettacamp.pro>
2022-10-17 12:58:41 +0700

    push

--- ts-day2.js
+++ ts-day2.js
@@ -1,8 +1,9 @@
 var Case1 = [1, "data", "3", "result"];
 var Case2 = ["Bejo", "has", "4", "sport", "car"];
-var hallo = function (text) {
+var print = function (text) {
     // metode join() mengembalikan array sebagai string.
     return text.join(" ");
 };
-console.log(hallo(Case1));
-console.log(hallo(Case2));
+console.log(print(Case1));
+console.log(print(Case2));
+//--------------------------------------------------------
--- ts-day2.ts
+++ ts-day2.ts
@@ -3,10 +3,76 @@ type data = (string | number)[] // alias
 let Case1:data =  [1, "data", "3", "result"]
 let Case2:data = ["Bejo", "has", "4", "sport", "car"]
 
-const hallo = (text:data):string => {
+const printz = (text:data):string => {
     // metode join() mengembalikan array sebagai string.
     return text.join(" ")
 }
 
-console.log(hallo(Case1));
-console.log(hallo(Case2));
+console.log(printz(Case1));
+console.log(printz(Case2));
+
+//--------------------------------------------------------
+
+// array
+let array:number[];
+array = [1,2,3,4,5]
+
+// tuple
+let biodata: [string, number, string];
+biodata = ["jogja",123, "111"]
+
+// object
+type User = {
+    name: string,
+    age: number
+    alamat: string
+};
+
+let user: User = {
+    name: "tegar",
+    age: 21,
+    alamat: "aaaa"
+}
+
+// any
+let hero:any = "batman"
+
+hero = 20;
+hero = [];
+hero = {};
+hero = true || false
+
+// enum (numeric)
+// enum month {
+//     jan = 1,
+//     feb,
+//     mar,
+//     apr,
+//     mei,
+//     jun,
+//     jul
+// }
+// console.log(month);
+
+// enum (string)
+enum month {
+    jan = "januari",
+    feb = "february",
+    mar = "maret",
+    apr = "april",
+    mei = "mei",
+    jun = "juni",
+    jul = "july"
+}
+console.log(month);
+
+// literal
+// let a =  "laki-laki" | "perempuan"
+
+// let b = "laki-laki"
+// let c = "perempuan"
+
+// union
+let phone: number | string;
+phone = 62811111;
+phone = "085728669512";
