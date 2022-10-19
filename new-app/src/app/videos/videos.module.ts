64936405ca414c65ca114049a149b4ab369235d3
MiftahulTegar <miftahul.tegar@zettacamp.pro>
2022-10-19 09:33:57 +0700

    push

---
+++ new-app/src/app/videos/videos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VideoComponent } from './video/video.component';
+
+
+
+@NgModule({
+  declarations: [
+    VideoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VideosModule { }
