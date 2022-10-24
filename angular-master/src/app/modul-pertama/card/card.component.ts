8cdd0340fd24c70b9ecef4cbce26ab8747ba1f09
zettacamp-miftahul-tegar <miftahul.tegar@zettacamp.pro>
2022-10-24 16:03:50 +0700

    push

--- angular-master/src/app/modul-pertama/card/card.component.ts
+++ angular-master/src/app/modul-pertama/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -10,7 +11,37 @@ export class CardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const subject = new Subject();
+    const series$ = subject.asObservable();
+
+    series$.subscribe(val => console.log("early sub:" + val));
+    
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+
+    setTimeout(() => {
+      series$.subscribe(val => console.log("late sub:" + val));
+      subject.next(4);
+    }, 3000);
+    
   }
+
+  // const subjectt = new BehaviorSubject(123);
+
+  // two new subscribers will get initial value => output: 123, 123
+  // subject.subscribe(console.log);
+  // subject.subscribe(console.log);
+
+  // two subscribers will get new value => output: 456, 456
+  // subject3.next(456);
+
+  // new subscriber will get latest value (456) => output: 456
+  // subject4.subscribe(console.log);
+
+  // all three subscribers will get new value => output: 789, 789, 789
+  // subject5.next(789);
+
   @Input() card: any;
   @Input() id!: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
