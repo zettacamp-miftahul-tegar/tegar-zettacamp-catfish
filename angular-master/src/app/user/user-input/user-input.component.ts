1131b8d25bc5800e7522afb2c636ef34ee8b3193
zettacamp-miftahul-tegar <miftahul.tegar@zettacamp.pro>
2022-10-28 14:17:47 +0700

    push

--- angular-master/src/app/user/user-input/user-input.component.ts
+++ angular-master/src/app/user/user-input/user-input.component.ts
@@ -1,9 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { UserService } from '../user.service';
-import { ActivatedRoute } from '@angular/router';
-import { User } from '../model/user.model';
-import { TranslateService } from '@ngx-translate/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormArray,
+  FormBuilder
+} from '@angular/forms';
+import {
+  UserService
+} from '../user.service';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  User
+} from '../model/user.model';
+import {
+  TranslateService
+} from '@ngx-translate/core';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-user-input',
@@ -11,27 +29,44 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./user-input.component.css']
 })
 export class UserInputComponent implements OnInit {
-  
+
   selectedUser: User | null | undefined;
   id: any;
   genders = ['male', 'female'];
   signupForm!: FormGroup;
-  bebas : any;
+  bebas: any;
 
   selectedLang = 'en';
+  isEdit: any;
+  subcription: any;
+  // 
 
-  constructor(private route: ActivatedRoute, private data: UserService, public translateService: TranslateService) {}
+  constructor(private route: ActivatedRoute, private data: UserService, public translateService: TranslateService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // const id = parseInt(this.route.snapshot.paramMap.get('id') !, 10);
-    // console.log(id)
-
-    this.data.userList$.subscribe(data => {
-      this.selectedUser = this.data.getBookById(this.id);
-    })
+    // this.data.userList$.subscribe(data => {
+    //   this.selectedUser = this.data.getBookById(this.id);
+    // })
     this.initForm();
+
+    if (this.isEdit) {
+      this.subcription = this.data.userList$
+        .pipe(first((users: User[]) => users.length !== 0))
+        .subscribe((users: any[]) => {
+          const user: any = users.find(user => user.id === this.id);
+
+          for (let i = 0; i < user.address.length; i++) {
+            this.onAddAddress();
+          }
+
+          this.signupForm.patchValue(User);
+        });
+    } else {
+      this.onAddAddress();
+    }
+
   }
-  
+
 
   initForm() {
     if (this.route.snapshot.params['id']) {
@@ -48,21 +83,18 @@ export class UserInputComponent implements OnInit {
       'position': new FormControl(null, Validators.required),
       'status': new FormControl(null, Validators.required),
       'genders': new FormControl(null, Validators.required),
-      'address': new FormGroup({
-        'address': new FormControl(null, Validators.required),
-        'zipcode': new FormControl(null, Validators.required),
-        'city': new FormControl(null, Validators.required),
-        'country': new FormControl(null, Validators.required)
-      })
+      'address': new FormArray([])
     });
     this.getData();
   }
 
   getData() {
-    this.data.userList$.subscribe(x=> {
+    this.data.userList$.subscribe(x => {
       this.bebas = x;
       // console.log(this.bebas);
-      let user = this.bebas.filter((x: { id: any; }) => x.id == this.id);
+      let user = this.bebas.filter((x: {
+        id: any;
+      }) => x.id == this.id);
       let a = this.signupForm.patchValue(user[0]);
     });
   }
@@ -84,4 +116,20 @@ export class UserInputComponent implements OnInit {
   setLanguage(lang: string) {
     this.translateService.use(lang);
   }
+
+  onAddAddress() {
+    let creds = this.signupForm.controls['address'] as FormArray;
+    creds.push(new FormGroup({
+      address: new FormControl(null, Validators.required),
+      zipcode: new FormControl(null, Validators.required),
+      city: new FormControl(null, Validators.required),
+      country: new FormControl(null, Validators.required)
+    }));
+
+  }
+
+  get controls() {
+    return (this.signupForm.get('address') as FormArray).controls;
+  }
+
 }
